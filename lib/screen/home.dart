ffcdfc39069fb5bd96b3c159476ef3115d9566a5
juhyoungshin <juhyoung200410@gmail.com>
2022-09-17 01:21:14 +0900

    Google Map Added

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:withyou/tool/drawer.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:withyou/tool/todoAdd.dart';
+
+
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -9,13 +17,75 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  var uid = FirebaseAuth.instance.currentUser?.email;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        iconTheme: const IconThemeData(color: Colors.grey),
+        backgroundColor: Colors.white,
+        elevation: 0,
       ),
-      drawer: DrawerForAll(),
 
-    );
+      drawer: DrawerForAll(),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Padding(
+          padding: EdgeInsets.only(left: 30, right: 30,),
+            child: Column(
+              children: [
+                Padding(padding: EdgeInsets.only(top: 20)),
+                TableCalendar(
+                  headerStyle: HeaderStyle(
+                    formatButtonVisible: false,
+                    titleCentered: true,
+                  ),
+                  firstDay: DateTime.utc(2010, 10, 16),
+                  lastDay: DateTime.utc(2030, 3, 14),
+                  focusedDay: DateTime.now(),
+                ),
+                Padding(padding: EdgeInsets.only(bottom:20)),
+                Align(
+                    child:Text('할 일'),
+                  alignment: Alignment.centerLeft,
+                ),
+                Padding(padding: EdgeInsets.only(bottom:20)),
+
+                StreamBuilder<QuerySnapshot>(
+                  stream: FirebaseFirestore.instance.collection('ToDo').where('uid', isEqualTo: uid).snapshots(),
+                  //orderBy("date", descending: true).
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return CircularProgressIndicator();
+                    }
+                    return ListView.builder(
+                        itemCount: snapshot.data?.docs.length,
+                        physics: NeverScrollableScrollPhysics(),
+                        shrinkWrap: true,
+                        itemBuilder: (context, index){
+                      return ListTile(
+                        title: Text('One-line with trailing widget'),
+                      );
+                    });
+                  }
+                ),
+
+
+
+              ],
+              crossAxisAlignment: CrossAxisAlignment.start,
+            )
+        ),),
+      backgroundColor: Colors.white,
+
+    floatingActionButton: FloatingActionButton(
+    onPressed: (){
+      Navigator.push(context,
+      MaterialPageRoute(builder: (context) => ToDoAdd()));
+      },
+    child: Icon(Icons.post_add_outlined, size: 25,),
+    backgroundColor: Color.fromRGBO(144, 210, 140, 1.0),
+    ));
   }
 }
---
+++ lib/tool/todoAdd.dart
@@ -0,0 +1,204 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+
+class ToDoAdd extends StatefulWidget {
+  const ToDoAdd({Key? key}) : super(key: key);
+
+  @override
+  State<ToDoAdd> createState() => _ToDoAddState();
+}
+
+class _ToDoAddState extends State<ToDoAdd> {
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  TextEditingController titleController = TextEditingController();
+  TextEditingController contentController = TextEditingController();
+  TextEditingController nameController = TextEditingController();
+
+  String postTitle = '';
+  String content = '';
+
+  String downloadUrl ='';
+
+  Future<void> _showMyDialog() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Cannot Post Your Writing'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: const <Widget>[
+                Text('Please fill out both title and content'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Ok'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<void> _showNameDialog() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Cannot Post Your Writing'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: const <Widget>[
+                Text('Please fill out your name'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Ok'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+
+
+    return Scaffold(
+      appBar: AppBar(),
+      bottomNavigationBar: BottomAppBar(
+          color: Colors.white,
+          elevation: 0,
+          child: Container(
+              height: 76.5,
+              child: Row(
+                children: <Widget>[
+                  Padding(
+                      padding: EdgeInsets.all(MediaQuery.of(context).size.width * 0.05, ),
+                      child: GestureDetector(
+                          onTap: (){
+                            if (FirebaseAuth.instance.currentUser == null){
+                              _showNameDialog();
+                            }
+                            else if (titleController.text == ''){
+                              _showMyDialog();
+                            }
+                            else {
+                              FirebaseFirestore.instance.collection('ToDo').doc().set({
+                                'title': postTitle,
+                                'content': content,
+                                'displayName': "${FirebaseAuth.instance.currentUser?.displayName}",
+                                'uid': "${FirebaseAuth.instance.currentUser?.email}",
+                                'imageUrl': null,
+                                'date': DateTime.now().toUtc(),
+                                'displayDate' : DateFormat.yMMMd('en_US').format(DateTime.now()),
+                                'login': (FirebaseAuth.instance.currentUser != null) ? true : false,
+                                'logoutName': nameController.text
+                              });
+                              Navigator.pop(context);
+
+                            }
+
+                          },
+
+                          child: Container(
+                            width: MediaQuery.of(context).size.width * 0.3,
+                            height: 53.5,
+                            child: Center(child: Text('Submit', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white, fontSize: 15),),),
+
+                            decoration: BoxDecoration(
+                                color: Color.fromRGBO(121, 179, 119, 1.0),
+                                borderRadius: BorderRadius.circular(20)
+                            ),
+                          )
+                      )
+                  ),
+                ],
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              )
+          )
+      ),
+        body: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Column(
+              children: <Widget>[
+                (FirebaseAuth.instance.currentUser == null) ? Padding(
+                  padding: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.02, left: MediaQuery.of(context).size.width *0.03, right:MediaQuery.of(context).size.width *0.4 ),
+                  child: TextField(
+                    controller: nameController,
+                    decoration: InputDecoration(
+                      labelText: 'Write your name here',
+                    ),
+                  ),
+                ): Padding(padding:EdgeInsets.zero),
+                Padding(
+                    padding: EdgeInsets.only(top: MediaQuery.of(context).size.height *0.05, left: MediaQuery.of(context).size.width *0.03),
+                    child: Text('Title', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),)
+                ),
+                Padding(
+                  padding: EdgeInsets.only( left: MediaQuery.of(context).size.width *0.03, right:MediaQuery.of(context).size.width *0.03 ),
+                  child: TextField(
+                    controller: titleController,
+                    maxLength: 50,
+                    decoration: InputDecoration(
+                        labelText: 'Add a title here'
+                    ),
+                    onChanged: (value){
+                      setState(() {
+                        postTitle = value;
+                      });
+                    },
+                  ),
+                ),
+
+                Padding(
+                    padding: EdgeInsets.only(top: MediaQuery.of(context).size.height *0.05, left: MediaQuery.of(context).size.width *0.03),
+                    child: Text('Content', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),)
+                ),
+                Padding(
+                  padding: EdgeInsets.only( left: MediaQuery.of(context).size.width *0.03, right:MediaQuery.of(context).size.width *0.03 ),
+                  child: TextField(
+                    keyboardType: TextInputType.multiline,
+                    controller: contentController,
+                    decoration: InputDecoration(
+                      labelText: 'Add your content here',
+                    ),
+                    maxLines: null,
+                    onChanged: (value){
+                      setState(() {
+                        content = value;
+                      });
+                    },
+                  ),
+                ),
+
+
+
+
+              ],
+              crossAxisAlignment: CrossAxisAlignment.start,
+            )
+        ));
+  }
+}
+

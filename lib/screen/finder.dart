f839a61f9692305ad136426325205e373bfc150b
eastdown <829dankim@gmail.com>
2022-09-16 17:09:07 +0900

    Google Map Added

--- lib/screen/finder.dart
+++ lib/screen/finder.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
 class Finder extends StatefulWidget {
   const Finder({Key? key}) : super(key: key);
 
@@ -6,8 +9,29 @@ class Finder extends StatefulWidget {
 }
 
 class _FinderState extends State<Finder> {
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Maps Sample App'),
+          backgroundColor: Colors.green[700],
+        ),
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 11.0,
+          ),
+        ),
+      ),
+    );
   }
 }

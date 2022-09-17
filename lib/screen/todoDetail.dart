5ea6b08b33b0dc78a84d9c37b9cd19c05c1f7d07
juhyoungshin <juhyoung200410@gmail.com>
2022-09-17 20:37:58 +0900

    Google Map Added

--- lib/screen/todoDetail.dart
+++ lib/screen/todoDetail.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:withyou/screen/home.dart';
+
+int postIndex = 0;
 
 class ToDoDetail extends StatelessWidget {
-  const ToDoDetail({Key? key}) : super(key: key);
+  ToDoDetail({Key? key}) : super(key: key);
+
+  var uid = FirebaseAuth.instance.currentUser?.email;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+      return StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance.collection('ToDo').where('uid', isEqualTo: uid).snapshots(),
+          //orderBy("date", descending: true).
+          builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          return Scaffold(
+            appBar: AppBar(),
+            body: SingleChildScrollView(
+              scrollDirection: Axis.vertical,
+              child: Column(
+                children: [
+                  Text('${snapshot.data!.docs[0]['title']}'),
+                  Text('${snapshot.data!.docs[0]['content']}'),
+                ],
+              ),
+            )
+          );
+        },
+      );
   }
 }
+
